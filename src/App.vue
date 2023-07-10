3b472f82c6c0b4c19a6a862cc9bd347df1592d92
Graig Janssen <graig.janssen@gmail.com>
2023-07-10 17:23:27 -0400

    starter column component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <kanban-column label="To-Do" />
+  <kanban-column label="In Progress" />
+  <kanban-column label="Done" />
 </template>
 
+<script lang="ts">
+import KanbanColumn from "./components/KanbanColumn.vue";
+export default {
+  components: {
+    KanbanColumn,
+  },
+};
+</script>
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  background-color: #444;
+  color: #eee;
 }
 </style>
---
+++ src/components/KanbanColumn.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="kanban-column">
+    <h2>{{ label }}</h2>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    label: String,
+  },
+};
+</script>
